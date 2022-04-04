dfe437342ddde6c878a07f57c6e484de72bb56f0
Kang Dongho <habizero@gmail.com>
2022-04-04 22:26:28 +0900

    fix: 필터링용 클래스 추가

--- AI/run.py
+++ AI/run.py
@@ -9,24 +9,46 @@ from torch.utils.data import DataLoader
 
 from utils import RecommendationDataset
 
+class Filtering_Data:
+    def __init__(self):
+        self.data = RecommendationDataset()
+        self.pd_file = self.data.csv_load()
+
+    def single_genre(self, genre):
+        print(self.pd_file[self.pd_file['장르'].str.contains(genre)]['장르'])
+
+    def many_or_genres(self, genre):
+        genre = '|'.join(genre)
+        print(self.pd_file.query(f'장르.str.contains("{genre}")', engine='python')['장르'])
+
+    def many_and_genres(self, genre):
+        print(self.pd_file[self.pd_file['장르'].map(lambda x: all(string in x for string in genre))]['장르'])
+
+    def filter_order(self, query, genre):
+        if query == '단일':
+            self.single_genre(genre)
+        elif query == '모두':
+            self.many_and_genres(genre)
+        else :
+            self.many_or_genres(genre)
+
 
 if __name__ == '__main__':
-    x = RecommendationDataset()
-    pd_file = x.csv_load()
+    test = Filtering_Data()
 
     filtering_genre = '애니메이션'
     print("단일 장르 필터링, 해당 장르가 포함된 영화의 장르")
     print("--------------------------------------------------------")
-    print(pd_file[pd_file['장르'].str.contains(filtering_genre)]['장르'])
+    test.filter_order('단일',filtering_genre)
 
+    filtering_genre_list = ['애니메이션', '판타지']
     print("\n\n\n\n")
     print("or 연산, 주어진 장르들 중 하나라도 포함된 영화의 장르")
     print("--------------------------------------------------------")
-    print(pd_file.query(f'장르.str.contains("{filtering_genre}|판타지")', engine='python')['장르'])
+    test.filter_order('ㅇㅇ',filtering_genre_list)
 
 
     print("\n\n\n\n")
     print("and 연산, 주어진 장르들 모두 포함된 영화의 장르")
     print("--------------------------------------------------------")
-    filtering_genre_list = ['애니메이션', '판타지']
-    print(pd_file[pd_file['장르'].map(lambda x: all(string in x for string in filtering_genre_list))]['장르'])
+    test.filter_order('모두',filtering_genre_list)
