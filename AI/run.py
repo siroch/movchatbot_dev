243c692d1558dcdf89e97356d785ffac360385ab
Kang Dongho <habizero@gmail.com>
2022-04-04 22:05:16 +0900

    fix: 불필요한 파일 제거, 필터링 기능 테스트

--- AI/run.py
+++ AI/run.py
@@ -1,47 +1,32 @@
 import argparse
 
+import matplotlib.pyplot as plt
 import torch
+import pandas as pd
+from torch import nn
+
 from torch.utils.data import DataLoader
 
-from model import ModelClass
 from utils import RecommendationDataset
 
 
-def inference(data_loader, model):
-    """ model inference """
-
-    model.eval()
-    preds = []
-
-    with torch.no_grad():
-        for users, items in data_loader:
-            predicted = model(users, items)
-
-            preds.extend(map(lambda t: t.item(), predicted))
-
-    return preds
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='2021 AI Final Project')
-    parser.add_argument('--load-model', default='model.pt', help="Model's state_dict")
-    parser.add_argument('--dataset', default='../data/csv', help='dataset directory')
-    parser.add_argument('--batch-size', default=16, help='test loader batch size')
+    x = RecommendationDataset()
+    pd_file = x.csv_load()
 
-    args = parser.parse_args()
+    filtering_genre = '애니메이션'
+    print("단일 장르 필터링, 해당 장르가 포함된 영화의 장르")
+    print("--------------------------------------------------------")
+    print(pd_file[pd_file['장르'].str.contains(filtering_genre)]['장르'])
 
-    # load dataset in test folder
-    test_data = RecommendationDataset(f'{args.dataset}/ratings.csv', train=False)
-    test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False)
+    print("\n\n\n\n")
+    print("or 연산, 주어진 장르들 중 하나라도 포함된 영화의 장르")
+    print("--------------------------------------------------------")
+    print(pd_file.query(f'장르.str.contains("{filtering_genre}|판타지")', engine='python')['장르'])
 
-    user, item = test_data.get_datasize()
 
-    # instantiate model
-    model = ModelClass(num_users=user, num_items=item, rank=10)
-    model.load_state_dict(torch.load(args.load_model))
-
-    # write model inference
-    preds = inference(test_loader, model)
-
-    with open('result.txt', 'w') as f:
-        f.writelines('\n'.join(map(str, preds)))
+    print("\n\n\n\n")
+    print("and 연산, 주어진 장르들 모두 포함된 영화의 장르")
+    print("--------------------------------------------------------")
+    filtering_genre_list = ['애니메이션', '판타지']
+    print(pd_file[pd_file['장르'].map(lambda x: all(string in x for string in filtering_genre_list))]['장르'])
--- AI/utils.py
+++ AI/utils.py
@@ -3,4 +3,12 @@ import pandas as pd
 
 class RecommendationDataset:
     def __init__(self):
-        self.data_pd = pd.read_csv("../data/movie_list.csv")
+        self.json_path = "../data/json"
+        self.csv_path = "../data/csv"
+        self.data_path = "../data"
+        self.movies_list = f"{self.data_path}/movie_list.csv"
+
+    def csv_load(self):
+        movie_list = pd.read_csv(self.movies_list)
+
+        return movie_list
