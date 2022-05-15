b234ef78cbcf8002caeac370b5fbebce13474a49
jiho5993 <jiho5993@naver.com>
2022-05-15 23:36:40 +0900

    directory: modify directory structure

--- dev_AI_test/run.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pandas as pd
-# f = open('./model.csv','r')
-# rdr = csv.reader(f)
-
-# print(rdr[1,1])
-
-csv_input = pd.read_csv("./model.csv")
-print(csv_input.loc[1508,f'{2818}'])
