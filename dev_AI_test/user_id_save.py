6fd3e2b571eef22fdbcf7caaaa9cf1fde812db36
Kang Dongho <habizero@gmail.com>
2022-05-21 22:08:21 +0900

    feat: 유저 데이터 저장

--- dev_AI_test/user_id_save.py
+++ dev_AI_test/user_id_save.py
@@ -1,12 +1,110 @@
 import pickle
 import pandas as pd
+import numpy as np
+import random
 import json
 
-test = [["6a4300e36a0f6498bd0da951924304208eaee4387c99a1a2f1324d671933a38911", "1100", "1,2,3,4,5"]]
+class DataSave:
+    def __init__(self, req):
+        self.User_req = req
+        self.user_req_id = self.User_req['userRequest']['user']['id']
+        self.user_input_data = self.User_req['userRequest']['utterance']
 
-x = ['UserRequestID', 'UserID', 'MovieID']
+        with open("datas.pickle","rb") as f:
+            self.Review_Datas_df = pickle.load(f)
 
-list_df = pd.DataFrame(test, columns=x)
+        with open("users.pickle","rb") as f:
+            self.User_Datas_df = pickle.load(f)
 
-with open("users.pickle","wb") as f:
-    pickle.dump(list_df, f)
+        self.isUser = True
+        self.user_data = self.User_Datas_df[self.User_Datas_df['UserRequestID'] == self.user_req_id]
+        try:
+            self.user_data.iloc[0]
+        except:
+            self.isUser = False
+
+    def new_user(self, movie_id):
+        new_user_id = str(self.Review_Datas_df.iloc[len(self.Review_Datas_df.index)-1]['UserID'] + 1)
+        new_user_data = {'UserRequestID':self.user_req_id, 'UserID':new_user_id, 'MovieID':movie_id}
+        self.User_Datas_df.loc[len(self.User_Datas_df.index)] = new_user_data
+
+        self.review_data(list(map(int, movie_id.split(','))).sort(), new_user_id)
+
+    def old_user(self, movie_id):
+        old_user_id = self.user_data.iloc[0]['UserID']
+        old_user_movie = self.user_data.iloc[0]['MovieID']
+        head = list(map(int, old_user_movie.split(',')))
+        tail = list(map(int, movie_id.split(',')))
+        new_item = []
+        print(head,tail)
+        for i in tail:
+            if i not in head:
+                head.append(i)
+                new_item.append(i)
+
+        head.sort()
+        new_item.sort()
+        movie = ','.join(list(map(str,head)))
+
+        old_user_data = {'UserRequestID':self.user_req_id, 'UserID':old_user_id, 'MovieID':movie}
+        self.User_Datas_df = self.User_Datas_df.drop(index=self.User_Datas_df.loc[self.User_Datas_df['UserRequestID'] == self.user_req_id].index)
+        self.User_Datas_df.loc[len(self.User_Datas_df.index)] = old_user_data
+        print(new_item)
+        self.review_data(new_item, old_user_id)
+
+    def review_data(self, movie_id, user_id): # movieid userid int rating str
+        columns = ['MovieName', 'MovieID', 'Genre', 'Rating', 'UserID', 'Review']
+        review = '리뷰 없음'
+        data = []
+        for i in movie_id:
+            movie = self.Review_Datas_df[self.Review_Datas_df['MovieID'] == i].iloc[0]
+            name = movie['MovieName']
+            genre = movie['Genre']
+            rating = random.randrange(7,11)
+            data.append([name, i, genre, rating, int(user_id), review])
+
+        df = pd.DataFrame(data, columns=columns)
+
+        self.Review_Datas_df = self.Review_Datas_df.append(df, ignore_index=True)
+
+    def save_data(self):
+        ''' 유저 쿼리 전문이 오는 방식에 따라 수정할 예정
+        movie_names = self.user_input_data.split(',')
+        idx = []
+        for i in movie_names:
+            try:
+                movie = self.Review_Datas_df[self.Review_Datas_df['MovieName'] == i].iloc[0]
+            except:
+                continue
+            idx.append(movie['MovieID'])
+        m_list = list(map(int, idx))
+        m_list.sort()
+        movie_id = ','.join(list(map(str,m_list)))
+        '''
+        movie_id = '9,10,11,12,13,14'
+        if self.isUser:
+            self.old_user(movie_id)
+        else:
+            self.new_user(movie_id)
+        return self.User_Datas_df, self.Review_Datas_df
+
+
+if __name__ == '__main__':
+    with open(f"./test.json", "r") as file:
+        User_req = json.load(file)
+
+    test = DataSave(User_req)
+    new_data = test.save_data()
+    new_user_dataset = new_data[0]
+    new_movie_dataset = new_data[1]
+    print(new_user_dataset)
+    print(new_movie_dataset[new_movie_dataset['UserID'] == 1100]) # 지금 임시 등록한 유저id
+
+    # 아래 코드는 실행하면 데이터를 덮어씌움
+    # with open("users.pickle","wb") as f:
+    #     pickle.dump(new_user_dataset, f)
+
+    # with open("datas.pickle","wb") as f:
+    #     pickle.dump(new_movie_dataset, f)
+
+
--- dev_AI_test/user_recommender.py
+++ dev_AI_test/user_recommender.py
@@ -1,5 +1,6 @@
 import pickle
 import pandas as pd
+import numpy as np
 import json
 
 class User_Recommender:
@@ -13,22 +14,46 @@ class User_Recommender:
 
         with open("users.pickle","rb") as f:
             self.User_Datas_df = pickle.load(f)
-        # 유저 식별자 아이디
+        # 유저 식별자 아이디, str형식
         self.user_req_id = self.User_req['userRequest']['user']['id']
         # 유저 데이터셋에서 식별자 아이디가 있는 row를 가져옴
+        # 데이터프레임형식이며, 고유아이디이기에 row는 1개
         self.user_data = self.User_Datas_df[self.User_Datas_df['UserRequestID'] == self.user_req_id]
-        # UserRequestID | UserID | MovieID의 DataFrame에서 학습용 아이디와 유저가 학습한 영화번호들을 가져옴
-        self.user_id = self.user_data['UserID']
-        self.user_movie = self.user_data['MovieID']
-        # 해당 유저의 영화별 평점 추정치 데이터를 가져옴
-        self.user_reco_data = self.Reco_Model_df[int(self.user_id)]
+
+        self.isUser = True
+        try:
+            self.user_data.iloc[0]
+        except:
+            self.isUser = False
+        
+        if self.isUser:
+            # UserRequestID | UserID | MovieID의 DataFrame에서 학습용 아이디와 유저가 학습한 영화번호들을 가져옴
+            # 둘 다 str형식
+            self.user_id = self.user_data.iloc[0]['UserID']
+            self.user_movie = self.user_data.iloc[0]['MovieID']
+            # 해당 유저의 영화별 평점 추정치 데이터를 가져옴
+            self.user_reco_data = self.Reco_Model_df[int(self.user_id)]
+
+        
 
     def user_estimate(self, item): # 상위 N(item)개의 영화를 (추정 평점, 영화 아이디) 형식으로 리턴해줌
         movie_datas = []
+        movie_index = []
         for i in range(len(self.user_reco_data)):
-            movie_datas.append((self.user_reco_data[i], i+1))
-        movie_datas.sort(key=lambda x:x[0], reverse=True)
-        return movie_datas[:item]
+            movie_datas.append(self.user_reco_data[i])
+            movie_index.append(i+1)
+
+        user_movies = self.user_movie.split(',')
+        for i in range(len(user_movies)):
+            idx = int(user_movies[i])
+            del movie_datas[idx-1-i]
+            del movie_index[idx-1-i]
+
+        result = []
+        for i in range(len(movie_datas)):
+            result.append((movie_datas[i], movie_index[i]))
+        result.sort(key=lambda x:x[0], reverse=True)
+        return result[:item]
 
     def genre_extract(self): # 해당유저의 추정값 기준으로 가장 많이 나오는 순서대로 장르를 리턴, [[장르1,n],[장르2,m],...], 장르는 str n은 int
         movie_list = self.user_estimate(20)
@@ -64,15 +89,16 @@ class User_Recommender:
         return reco_movie_name
 
 
-
-
 if __name__ == '__main__':
     with open(f"./test.json", "r") as file:
         User_req = json.load(file)
 
     test = User_Recommender(User_req)
 
-    print(test.genre_extract())
-    print(test.recommender_movie(5))
+    if test.isUser:
+        print(test.genre_extract())
+        print(test.recommender_movie(5))
+    else:
+        print(None)
 
 
